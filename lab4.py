5d7d3e2335bca282eccf442ef3452eba0c94cb48
bbubbush <bbubbush@naver.com>
2018-01-06 23:32:11 +0900

    2018-01-06
    오랜만에 하는거 같은 텐서

--- lab4.py
+++ lab4.py
@@ -80,10 +80,39 @@ for step in range(20001):
 
 # 이번엔 파일에서 데이터 읽어오기
 import numpy as np
-
+tf.set_random_seed(777)
 xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+
+# 2차원 data slicing 방법 익히기
 x_data = xy[:, 0:-1]
 y_data = xy[:, [-1]]
 
 print(x_data.shape, x_data, len(x_data))
 print(y_data.shape, y_data)
+
+X = tf.placeholder(tf.float32, shape = [None, 3])
+Y = tf.placeholder(tf.float32, shape = [None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = tf.matmul(X, W) + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 1e-5)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+	cost_val, hy_val, _ = sess.run([cost, hypothesis, train], feed_dict={X:x_data, Y:y_data})
+	if step % 10 == 0:
+		print(step, "Cost : ", cost_val, "\nPrediction :\n", hy_val)
+
+print("Your score will be ", sess.run(hypothesis, feed_dict={X:[[100, 70, 101]]}))
+print("Other scorees will be  ", sess.run(hypothesis, feed_dict={X:[[60, 70, 110], [90, 100, 80]]}))
+
+# 여러 파일에서 읽어와서 처리하는 방법도 있지만 당장 필요없어서 패스
