6366e2f6f90e8814a253cb48e3611bbadc5afed2
bbubbush <bbubbush@naver.com>
2018-01-15 15:22:18 +0900

    2018-01-15

--- lab7-3.py
+++ lab7-3.py
@@ -21,8 +21,9 @@ hypothesis = tf.nn.softmax(tf.matmul(X, W) + b)
 cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(hypothesis), axis = 1))		# if axis is 0 then return values is sum columns. 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.1).minimize(cost)
 
-is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))	# tf.equal's return type is bool. True is , False is 0
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.int32) * 100)	# bool type change to float32. but change to int32 is fine if multiply 100.
+# accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32)) 와 위의 코드는 같다.
 
 training_epochs = 15
 batch_size = 100
@@ -39,15 +40,22 @@ with tf.Session() as sess:
 			c, _ = sess.run([cost, optimizer], feed_dict = {X: batch_xs, Y: batch_ys})
 			avg_cost += c / total_batch
 
-		print('Epoch:', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(avg_cost))
+		print('Epoch:', '%02d' % (epoch + 1), 'cost = ', '{:.9f}'.format(avg_cost))
 
 	print("Learning Finished!")
 
+	'''
+	accuracy.eval은 sess.run을 활용할 때, 넘기는 값이 1개일 때 간단하게 사용할 수 있다.
+	sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels})와 동일하게 사용 가능
+	'''
 	print("Accuracy: ", accuracy.eval(session=sess, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
 
-	r = random.randint(0, mnist.test.num_examples - 1)
-	print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
-	print("Prediction: ", sess.run(tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r+1]}))
+	# 0부터 num_examples의 전체 개수 사이에서 랜덤하게 하나 선택. size인자를 넣으면 배열형태로 return 가능
+	r = random.randint(0, mnist.test.num_examples - 1)	
+	print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))	# True data
+
+	# mnist.test.images를 직접 보고 싶은데 어디서 찾아야하는지 모르겠다. 
+	print("Prediction: ", sess.run(tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r+1]}))	# Prediction data
 
 	plt.imshow(
 		mnist.test.images[r:r+1].reshape(28, 28),
